1636f91c3efcfcae583b58be7e246d7f16acaccf
HÁ <haidee.alvarez.c@gmail.com>
2025-08-08 09:26:35 +0100

    apple pic

--- index.js
+++
@@ -1,5 +0,0 @@
-font-family: 'font-main';
-  font-stretch: 10% - 90%; /* Note that font-stretch is a % of normal width */
-  font-style: NORMAL; /* See note below */
-  font-weight: 2-89;
-  src: url(brain.png) format('png');
